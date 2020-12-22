39006d2377028055b634af0c44af27bd4ac188c5
Samvw90 <45743681+Samvw90@users.noreply.github.com>
2020-12-21 17:13:39 -0800

    first commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user has connected');
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(4000, () => {
+  console.log('listeningon on *:4000');
+});
